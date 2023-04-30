f35a60ef2dff473f8834906207a9e411e71ff289
andrew <andrewnijmeh1@gmail.com>
2023-04-30 16:34:38 -0400

    accept connection works

--- src/main.rs
+++ src/main.rs
@@ -60,6 +60,33 @@ async fn proxy_request(client: &Client<HttpConnector>, server: &str, request: Re
     Ok(response_str)
 }
 
+async fn handle_request(load_balancer: Arc<LoadBalancer>, client:Client<HttpConnector>, mut stream: TcpStream) -> Result <(), String> {
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer).map_err(|e| format!("Failed to read from stream: {}", e))?;
+    let request_str = String::from_utf8_lossy(&buffer[..]).to_string();
+    let request = Request::builder()
+        .method(Method::POST)
+        .uri("/echo")
+        .header("content-type", "application/json")
+        .body(Body::from(request_str.clone()))
+        .map_err(|e| format!("Failed to build request: {}", e))?;
+    let server = load_balancer.get_server();
+    let response_str = proxy_request(&client, &server, request).await?;
+    let response = format!("{} response from server {}: {} \n", request_str, server, response_str);
+    stream.write(response.as_bytes()).map_err(|e| format!("Failed to write to stream: {}", e))?;
+    Ok(())
+}
+async fn accept_connection(load_balancer: Arc<LoadBalancer> , client: Client<HttpConnector>, mut receiver: Receiver<TcpStream>) {
+    while let Some(stream) = receiver.recv().await {
+        let load_balancer = load_balancer.clone();
+        let client = client.clone();
+        tokio::spawn(async move {
+            if let Err(e) = handle_request(load_balancer, client, stream).await {
+                error!("{}", e);
+            }
+        });
+    }
+}
 fn main() {
     println!("placeholder")
 }
