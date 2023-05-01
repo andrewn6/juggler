ded44483bd4465db9bd190b790dd35c988b68b26
Andrew Nijmeh <andrewnijmeh1@gmail.com>
2023-05-01 10:46:54 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,1 @@
-./target/release/ntunnel --port 8080 --servers http://localhost:8081,http:/│
-/localhost:8082
+./target/release/ntunnel --port 8080 --servers http://localhost:8081,http:/localhost:8082
