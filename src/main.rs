a46fa913795903663be567ea657583e5376f7219
andrew <andrewnijmeh1@gmail.com>
2023-05-02 22:43:44 -0400

    yew starter

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,54 @@
-use std::net::{TcpListener, TcpStream};
-use std::io::{Write, Read};
-use std::thread;
-use std::sync::{Arc, Mutex};
+use env_logger::Env;
+use yew::prelude::*;
+
 use hyper::body::HttpBody;
-use structopt::StructOpt;
-use serde::Deserialize;
-use hyper::{Client, Request, Body, Method};
 use hyper::client::connect::HttpConnector;
 use hyper::header::HeaderValue;
+use hyper::{Body, Client, Method, Request};
+
+use log::{error, info};
+use serde::Deserialize;
+
+use std::io::{Read, Write};
+use std::net::{TcpListener, TcpStream};
+use std::sync::{Arc, Mutex};
+use std::thread;
+
+use structopt::StructOpt;
 use tokio::runtime::Builder;
 use tokio::sync::mpsc::{channel, Receiver};
-use log::{info, error};
-use env_logger::Env;
+
+#[function_component]
+fn App() -> Html {
+    let counter = use_state(|| 0);
+    let onclick = {
+        let counter = counter.clone();
+        move |_| {
+            let value = *counter + 1;
+            counter.set(value);
+        }
+    };
+
+    html! {
+        <div>
+            <button {onclick}>{ "+1" }</button>
+            <p>{ *counter }</p>
+        </div>
+    }
+}
 
 #[derive(StructOpt)]
 #[structopt(name = "load_balancer")]
 struct Opt {
     #[structopt(short = "p", long = "port", default_value = "8080")]
     port: u16,
-    #[structopt(short = "s", long = "servers", default_value = "http://localhost:8081,http://localhost:8082")]
+    #[structopt(
+        short = "s",
+        long = "servers",
+        default_value = "http://localhost:8081,http://localhost:8082"
+    )]
     servers: Vec<String>,
-    #[structopt(short = "w" , long = "weights", default_value = "1,1")]
+    #[structopt(short = "w", long = "weights", default_value = "1,1")]
     weights: Vec<usize>,
 }
 
@@ -62,24 +90,38 @@ impl LoadBalancer {
 
 /* Proxy request */
 async fn proxy_request(
-    client: &Client<HttpConnector>, server: &str, request: Request<Body>
+    client: &Client<HttpConnector>,
+    server: &str,
+    request: Request<Body>,
 ) -> Result<String, String> {
     let mut proxy_request = request;
     *proxy_request.uri_mut() = server.parse().map_err(|e| format!("Invalid URI: {}", e))?;
-    proxy_request.headers_mut().insert("host", HeaderValue::from_str(server).unwrap());
-    let response = client.request(proxy_request).await.map_err(|e| format!("Error: {}", e))?;
-    let response_body = response.into_body().map_err(|e| format!("Failed to read response body: {:?}", e));
-    let response_bytes = hyper::body::to_bytes(response_body).await.map_err(|e| format!("Failed to read response bytes: {:?}", e))?;
+    proxy_request
+        .headers_mut()
+        .insert("host", HeaderValue::from_str(server).unwrap());
+    let response = client
+        .request(proxy_request)
+        .await
+        .map_err(|e| format!("Error: {}", e))?;
+    let response_body = response
+        .into_body()
+        .map_err(|e| format!("Failed to read response body: {:?}", e));
+    let response_bytes = hyper::body::to_bytes(response_body)
+        .await
+        .map_err(|e| format!("Failed to read response bytes: {:?}", e))?;
     let response_str = String::from_utf8_lossy(&response_bytes).to_string();
     Ok(response_str)
 }
 
-async fn handle_request
-(
-load_balancer: Arc<LoadBalancer>, client:Client<HttpConnector>, mut stream: TcpStream
-) -> Result <(), String> {
+async fn handle_request(
+    load_balancer: Arc<LoadBalancer>,
+    client: Client<HttpConnector>,
+    mut stream: TcpStream,
+) -> Result<(), String> {
     let mut buffer = [0; 1024];
-    stream.read(&mut buffer).map_err(|e| format!("Failed to read from stream: {}", e))?;
+    stream
+        .read(&mut buffer)
+        .map_err(|e| format!("Failed to read from stream: {}", e))?;
     let request_str = String::from_utf8_lossy(&buffer[..]).to_string();
     let request = Request::builder()
         .method(Method::POST)
@@ -96,10 +138,13 @@ load_balancer: Arc<LoadBalancer>, client:Client<HttpConnector>, mut stream: TcpS
         }
     };
 
-    let response_data: RequestData = serde_json::from_str(&response_str).map_err(|e| format!("Failed to parse response: {}", e))?;
+    let response_data: RequestData = serde_json::from_str(&response_str)
+        .map_err(|e| format!("Failed to parse response: {}", e))?;
     let response_message = response_data.message;
     let response = format!("Response from server {}:\n", response_message);
-    stream.write(response.as_bytes()).map_err(|e| format!("Failed to write to stream: {}", e))?;
+    stream
+        .write(response.as_bytes())
+        .map_err(|e| format!("Failed to write to stream: {}", e))?;
 
     Ok(())
 }
@@ -122,14 +167,22 @@ async fn accept_connection(
 
 #[tokio::main]
 async fn main() -> Result<(), String> {
+    yew::Renderer::<App>::new().render();
+    
+    // Main logic, uses handle_request and runs load balancer
     let opt = Opt::from_args();
     env_logger::from_env(Env::default().default_filter_or("info")).init();
-    let listener = TcpListener::bind(format!("localhost:{}", opt.port)).map_err(|e| format!("Failed to bind to port {}: {:?}", opt.port, e))?;
+    let listener = TcpListener::bind(format!("localhost:{}", opt.port))
+        .map_err(|e| format!("Failed to bind to port {}: {:?}", opt.port, e))?;
 
     let (sender, receiver) = channel::<TcpStream>(1024);
     let client = Client::new();
-    
-    let opt_servers = vec!["server1".to_string(), "server2".to_string(), "server3".to_string()];
+
+    let opt_servers = vec![
+        "server1".to_string(),
+        "server2".to_string(),
+        "server3".to_string(),
+    ];
     let servers = opt_servers.clone();
     for i in 0..num_cpus::get() {
         let receiver = tokio::sync::mpsc::channel(1024).1;
@@ -149,7 +202,10 @@ async fn main() -> Result<(), String> {
     }
 
     while let Ok(stream) = listener.accept() {
-        sender.send(stream.0).await.map_err(|e| format!("Failed to send connection to worker: {:?}", e))?;
+        sender
+            .send(stream.0)
+            .await
+            .map_err(|e| format!("Failed to send connection to worker: {:?}", e))?;
     }
 
     Ok(())
